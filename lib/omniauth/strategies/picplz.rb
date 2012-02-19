7a377f2017bd2551e2d827ade875c5ecc9442bda
Lucas Allan <lucas.allan@gmail.com>
2012-02-19 15:56:29 -0300

    getting user information

--- lib/omniauth/strategies/picplz.rb
+++ lib/omniauth/strategies/picplz.rb
@@ -8,11 +8,26 @@ module OmniAuth
         :authorize_url => 'https://picplz.com/oauth2/authenticate',
         :token_url => "https://picplz.com/oauth2/access_token"
       }
-      
-      def request_phase
-        super
+
+      info do
+        {
+          :name => raw_info['display_name'],
+          :uid => raw_info['id'],
+          :username => raw_info['username']
+        }
       end
 
+      def raw_info
+        access_token.options[:mode] = :query
+        access_token.options[:param_name] = :oauth_token
+        user_info = access_token.get('https://api.picplz.com/api/v2/user.json?id=self')
+        @raw_info ||= user_info.parsed['value']['users'].last
+      end
+
+      
+      def client_params
+        {:client_id => options[:client_id], :redirect_uri => callback_url ,:response_type => "code"}
+      end
     end
   end
 end
